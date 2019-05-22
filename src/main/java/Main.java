c930a51e6caf46130c895dd3b9faca55f12305a8
Julian Benitez <jubenitez@itba.edu.ar>
2019-05-22 16:59:12 -0300

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.biojava.nbio.core.sequence.io.ProteinSequenceCreator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+//        ProteinSequenceCreator
+    }
+}
